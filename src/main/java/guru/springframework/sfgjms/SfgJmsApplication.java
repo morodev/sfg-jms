dd8616bbdfb66a67551968fe1a223e88626c62c9
Luca Moro <71976553+morodev@users.noreply.github.com>
2021-02-07 17:29:11 +0100

    181. Using Local ActiveMQ Broker with Spring Boot

--- src/main/java/guru/springframework/sfgjms/SfgJmsApplication.java
+++ src/main/java/guru/springframework/sfgjms/SfgJmsApplication.java
@@ -1,12 +1,5 @@
 package guru.springframework.sfgjms;
 
-import org.apache.activemq.artemis.api.core.TransportConfiguration;
-import org.apache.activemq.artemis.core.config.Configuration;
-import org.apache.activemq.artemis.core.config.impl.ConfigurationImpl;
-import org.apache.activemq.artemis.core.remoting.impl.netty.NettyAcceptorFactory;
-import org.apache.activemq.artemis.core.server.ActiveMQServer;
-import org.apache.activemq.artemis.core.server.ActiveMQServers;
-import org.apache.activemq.artemis.core.server.impl.ActiveMQServerImpl;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -22,14 +15,15 @@ public class SfgJmsApplication {
 				.setSecurityEnabled(false)
 				.addAcceptorConfiguration("invm", "vm://0"));*/
 
-		Configuration config = new ConfigurationImpl();
+		/*Configuration config = new ConfigurationImpl();
 		config.setPersistenceEnabled(false);
 		config.setSecurityEnabled(false);
 		config.setJournalDirectory("target/data/journal");
 		config.getAcceptorConfigurations().add(new TransportConfiguration(NettyAcceptorFactory.class.getName()));
-		ActiveMQServer server = new ActiveMQServerImpl(config);
+		ActiveMQServer server = new ActiveMQServerImpl(config);*/
 
 		// ActiveMQServers.newActiveMQServer(config);
+
 		SpringApplication.run(SfgJmsApplication.class, args);
 
 	}
