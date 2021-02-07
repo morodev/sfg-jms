076952557b7492a18cee63743ee9f80f692dc60a
Luca Moro <71976553+morodev@users.noreply.github.com>
2021-02-07 10:53:31 +0100

    176. Message Converter Configuration

--- src/main/java/guru/springframework/sfgjms/SfgJmsApplication.java
+++ src/main/java/guru/springframework/sfgjms/SfgJmsApplication.java
@@ -1,8 +1,12 @@
 package guru.springframework.sfgjms;
 
+import org.apache.activemq.artemis.api.core.TransportConfiguration;
+import org.apache.activemq.artemis.core.config.Configuration;
 import org.apache.activemq.artemis.core.config.impl.ConfigurationImpl;
+import org.apache.activemq.artemis.core.remoting.impl.netty.NettyAcceptorFactory;
 import org.apache.activemq.artemis.core.server.ActiveMQServer;
 import org.apache.activemq.artemis.core.server.ActiveMQServers;
+import org.apache.activemq.artemis.core.server.impl.ActiveMQServerImpl;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -11,12 +15,19 @@ public class SfgJmsApplication {
 
 	public static void main(String[] args) throws Exception {
 
-		ActiveMQServers.newActiveMQServer(new ConfigurationImpl()
+
+		/*ActiveMQServers.newActiveMQServer(new ConfigurationImpl()
 				.setPersistenceEnabled(false)
 				.setJournalDirectory("target/data/journal")
 				.setSecurityEnabled(false)
-				.addAcceptorConfiguration("invm", "vm://0"));
+				.addAcceptorConfiguration("invm", "vm://0"));*/
 
+		Configuration config = new ConfigurationImpl();
+		config.setPersistenceEnabled(false);
+		config.setSecurityEnabled(false);
+		config.setJournalDirectory("target/data/journal");
+		config.getAcceptorConfigurations().add(new TransportConfiguration(NettyAcceptorFactory.class.getName()));
+		ActiveMQServer server = new ActiveMQServerImpl(config);
 
 		SpringApplication.run(SfgJmsApplication.class, args);
 
---
+++ src/main/java/guru/springframework/sfgjms/config/JmsConfig.java
@@ -0,0 +1,23 @@
+package guru.springframework.sfgjms.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.jms.support.converter.MappingJackson2MessageConverter;
+import org.springframework.jms.support.converter.MessageConverter;
+import org.springframework.jms.support.converter.MessageType;
+
+/**
+ * Created By Luca Moro on 07/02/2021 10:33
+ */
+@Configuration
+public class JmsConfig {
+
+    @Bean
+    public MessageConverter messageConverter() {
+        MappingJackson2MessageConverter converter = new MappingJackson2MessageConverter();
+        converter.setTargetType(MessageType.TEXT);
+        converter.setTypeIdPropertyName("_type");
+        return converter;
+
+    }
+}
